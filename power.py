99c4d3fb44bf407b2989d83410dc52a04fb18843
akshay7805 <akshaygahlyan49@gmail.com>
2025-02-02 14:50:09 +0530

    Update power.py

--- power.py
+++ power.py
@@ -3,18 +3,18 @@ import os
 def shutdown():
      if(platform.system()=="Windows"):
         os.system('shutdown /s /t 0')
-     elif(platform.system=="Darwin"or platform.sytem=="Linux"):
-        os.system('shutdown now')
+     elif(platform.system()=="Darwin"or platform.sytem()=="Linux"):
+        os.system('sudo shutdown now')
      else:
         print("system is not supported")
 def restart():
      if(platform.system=="Windows"):
         os.system('shutdown /r /t 0')
      elif(platform.system=="Darwin" or platform.system=="Linux"):
-         os.system('reboot now')
+         os.system('sudo reboot ')
      else:
          print("system not supported")
-cmd=input("Enter the \'s for shutdown and \'r for restart system")
+cmd=input("Enter the command 's' for shutdown and 'r' for restart system":)
 if(cmd=='s'):
     shutdown()
 elif(cmd=='r'):
